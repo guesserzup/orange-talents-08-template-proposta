0594c4cd5a597e4c0dc44a5efba04a67a98c70cf
Gabriel Guesser <gabriel.guesser@zup.com.br>
2021-09-24 10:47:06 -0300

    retirando passwordEncoder da application class

--- src/main/java/br/com/zupacademy/propostas/PropostasApplication.java
+++ src/main/java/br/com/zupacademy/propostas/PropostasApplication.java
@@ -3,10 +3,7 @@ package br.com.zupacademy.propostas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -14,10 +11,5 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class PropostasApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PropostasApplication.class, args);
-	}
-
-	@Bean
-	public PasswordEncoder encoder() {
-		return new BCryptPasswordEncoder();
 	}
 }
